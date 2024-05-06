8b2acefb4a3fc6ed32966f7a1a36e71c59225a68
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-05-07 00:47:00 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Hamdy-MohamedCV.pdf";
+import pdf from "../../Assets/../Assets/Hamdy-Mohamedcv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
